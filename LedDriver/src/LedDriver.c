7516946f9febfb89538b6beeb122367f23fb771f
yakihiro@ac.auone-net.jp <yakihiro@ac.auone-net.jp>
2015-06-25 00:57:01 +0900

    refactoring

--- LedDriver/src/LedDriver.c
+++ LedDriver/src/LedDriver.c
@@ -2,6 +2,11 @@
 
 static uint16_t *ledsAddress;
 
+enum {
+	ALL_LEDS_OFF = 0,
+	ALL_LEDS_ON = ~ALL_LEDS_OFF
+};
+ 
 static uint16_t convertLedNumberToBit(int ledNumber)
 {
 	return 1 << (ledNumber - 1);
@@ -29,6 +34,6 @@ void LedDriver_TurnOff(int ledNumber)
 
 void LedDriver_TurnAllOn(void)
 {
-	*ledsAddress = 0xffff; 
+	*ledsAddress = ALL_LEDS_ON; 
 }
 
